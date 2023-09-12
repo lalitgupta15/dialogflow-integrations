7ca5c6120051c3f611d1904d14ab8c9a29da6a65
lalitgupta15 <38806152+lalitgupta15@users.noreply.github.com>
2023-09-12 17:10:28 +0530

    Update server.js

--- spark/server.js
+++ spark/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 // Insert your values here
 const sparkAccessToken ='Place your spark personal access token here';
 const targetUrl = 'Place you servers URL here';
-const projectId = 'Place your dialogflow projectId here';
+const projectId = testquestion-qvfy;
 
 const sessionClient = new dialogflowSessionClient(projectId);
 
